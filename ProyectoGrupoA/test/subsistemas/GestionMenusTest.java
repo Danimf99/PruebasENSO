2c8959f610797e03c3b2175b02d921359f9b3e4a
ManuLeston <mlestonleston@gmail.com>
2020-05-09 11:56:19 +0200

    Implementacion de test de escoger Plato

--- ProyectoGrupoA/test/subsistemas/GestionMenusTest.java
+++ ProyectoGrupoA/test/subsistemas/GestionMenusTest.java
@@ -2,23 +2,113 @@ package subsistemas;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import bean.Plato;
+import interfaces.ItfSensores;
+
 class GestionMenusTest {
+	
+	/**
+	 * Codigo de las pruebas de caja negra asociadas al método escoger plato.
+	 * @author Manuel Leston
+	 *
+	 */
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	
+	GestorSeleccionMenuImpl smenu;
+	GestionMenus gestorMenus;
+	GestionDatosImpl gestorDatos;
+	
+	@DisplayName("Test de clases validas")
+	@Nested
+	class PruebasValidas{
+		@BeforeEach
+		void setUp() throws Exception {
+			gestorDatos = new GestionDatosImpl();
+			gestorMenus = new GestionMenus(gestorDatos);
+			//Creamos el menu para este dia
+			gestorMenus.crearMenu("Albor",1);
+		}
+
+		@AfterEach
+		void tearDown() throws Exception {
+			gestorDatos = null;
+			gestorMenus = null;
+			smenu = null;
+		}
+		
+		@DisplayName("Escoger platos - CEP0")
+		@Test
+		void testEscogerPlatos() {
+			Assertions.assertDoesNotThrow(()->{gestorMenus.escogerPlatos(1, 1, 1);}, "Ha habido una excepcion");
+		}
 	}
+	
+	
+	@DisplayName("Test de clases no validas")
+	@Nested
+	class PruebasNoValidas{
+		@BeforeEach
+		void setUp() throws Exception {
+			gestorDatos = new GestionDatosImpl();
+			gestorMenus = new GestionMenus(gestorDatos);
+			//Creamos el menu para este dia
+			gestorMenus.crearMenu("Albor",1);
+		}
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+		@AfterEach
+		void tearDown() throws Exception {
+			gestorDatos = null;
+			gestorMenus = null;
+			smenu = null;
+		}
+		
+		@DisplayName("Escoger platos (Tipo fuera de rango) - CEP1")
+		@Test
+		void testEscogerPlatoTipo() {
+			assertThrows(Exception.class, ()->{
+				gestorMenus.escogerPlatos(1, 1, 4);
+			}, "Se esperaba excepcion por tipo fuera de rango");
+		}
+		
+		@DisplayName("Escoger platos (Tipo con valor negativo) - CEP2")
+		@Test
+		void testEscogerPlatoTipoNegativo() {
+			assertThrows(Exception.class, ()->{
+				gestorMenus.escogerPlatos(1, 1, -1);
+			}, "Se esperaba excepcion por tipo negativo");
+		}
+		
+		@DisplayName("Escoger platos (ID con valor negativo) - CEP3")
+		@Test
+		void testEscogerPlatoIDNegativo() {
+			assertThrows(Exception.class, ()->{
+				gestorMenus.escogerPlatos(-1, 1, 1);
+			}, "Se esperaba excepcion por ID negativo");
+		}
+		
+		@DisplayName("Escoger platos (Menu con valor negativo) - CEP4")
+		@Test
+		void testEscogerPlatoMenuNegativo() {
+			assertThrows(Exception.class, ()->{
+				gestorMenus.escogerPlatos(1, -23, 1);
+			}, "Se esperaba excepcion por Menu negativo");
+		}
+		
+		@DisplayName("Escoger platos (Menu con valor fuera de rango) - CEP5")
+		@Test
+		void testEscogerPlatoMenu() {
+			assertThrows(Exception.class, ()->{
+				gestorMenus.escogerPlatos(1, 23, 1);
+			}, "Se esperaba excepcion por Menu fuera de rango");
+		}
 	}
 
 }
